1729cc5a0ab345b244f775f5a7a3e1c78912263f
null-d-e-v <exception.nulldev@gmail.com>
2024-10-16 08:32:34 -0500

    [UPDATE] change format of middleware logger

--- src/middlewares/logger.ts
+++ src/middlewares/logger.ts
@@ -2,7 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import logger from "../utils/logger";
 
 export const httpLogger = (req: Request, res: Response, next: NextFunction) => {
-  const format = `METHOD:${req.method} PATH:${req.url} IP:${req.socket.remoteAddress}`
+  const format = `[method] ${req.method} | [path] ${req.url} | [ip] ${req.socket.remoteAddress}`
   logger.info(format)
   next()
 }
